44455f4eff4964d961f5194927708c930f54d058
Ishika <ishiika.ydv@gmail.com>
2025-05-20 14:43:57 +0530

    "rm virtual"

--- shell.nix
+++ shell.nix
@@ -2,12 +2,13 @@
 
 pkgs.mkShell {
   buildInputs = [
-    pkgs.python311Full
-    pkgs.nodejs_18
-    pkgs.python311Packages.pip
-    pkgs.python311Packages.setuptools
-    pkgs.python311Packages.wheel
-  ];
+  pkgs.python311Full
+  pkgs.nodejs_18
+  pkgs.python311Packages.pip
+  pkgs.python311Packages.setuptools
+  pkgs.python311Packages.wheel
+];
+
 
   shellHook = ''
     echo "✅ Custom Nix environment loaded"
