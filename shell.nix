42923eb7ff3848978c978bc5110e1647b2e9d804
Ishika <ishiika.ydv@gmail.com>
2025-05-20 14:32:47 +0530

    "nix.shell"

---
+++ shell.nix
@@ -0,0 +1,15 @@
+{ pkgs ? import <nixpkgs> {} }:
+
+pkgs.mkShell {
+  buildInputs = [
+    pkgs.python311Full
+    pkgs.nodejs_18
+    pkgs.python311Packages.pip
+    pkgs.python311Packages.setuptools
+    pkgs.python311Packages.wheel
+  ];
+
+  shellHook = ''
+    echo "✅ Custom Nix environment loaded"
+  '';
+}
